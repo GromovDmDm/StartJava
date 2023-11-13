dd1344bc2b366f89d1ddd2a54156ba6c14ffed92
Dmitriy <gromovv92@mail.ru>
2023-11-13 19:36:47 +0300

    improvements

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "man";
+    String name = "Ivan";
+    float height = 1.85f;
+    float weight = 84.5f;
+    int age = 31;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    void sit() {
+        System.out.println("siting");
+    }
+
+    void run() {
+        System.out.println("runing");
+    }
+
+    void speak() {
+        System.out.println("speaking");
+    }
+
+    void learnJava() {
+        System.out.println("learning java");
+    }
+}
