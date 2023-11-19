f6bd00c3f1147868ea923143577a5b32e0dd76e5
Dmitriy <gromovv92@mail.ru>
2023-11-19 21:01:45 +0300

    improvements

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,38 +2,42 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 
+    private Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         invokeCalculator();
     }
 
     private static void invokeCalculator() {
         Calculator calculator = new Calculator();
-        calculator.setA(Integer.parseInt(getUserMessage("Введите первое число: ")));
-        char sign = getUserMessage("Введите знак математической операции: ").charAt(0);
-        calculator.setB(Integer.parseInt(getUserMessage("Введите второе число: ")));
+        CalculatorTest сalculatorTest = new CalculatorTest();
+        calculator.setA(Integer.parseInt(inputInConsole("Введите первое число: ",
+                сalculatorTest.scanner)));
+        char sign = inputInConsole("Введите знак математической операции: ",
+                сalculatorTest.scanner).charAt(0);
+        calculator.setB(Integer.parseInt(inputInConsole("Введите второе число: ",
+                сalculatorTest.scanner)));
         try {
             calculator.calculate(sign);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-        finishCalculation();
+        decideContinue(сalculatorTest.scanner);
     }
 
-    private static String getUserMessage(String message) {
-        System.out.print(message);
-        Scanner scanner = new Scanner(System.in);
-        String userMessage = scanner.nextLine();
-        return userMessage;
-    }
-
-    private static void finishCalculation() {
-        switch(getUserMessage("Хотите продолжить вычисления? [yes/no]: ")) {
+    private static void decideContinue(Scanner scanner) {
+        switch(inputInConsole("Хотите продолжить вычисления? [yes/no]: ", scanner)) {
             case "yes": 
                 invokeCalculator();
             case "no": 
                 return;
             default: 
-                finishCalculation();
+                decideContinue(scanner);
         }
+    }
+
+    private static String inputInConsole(String message, Scanner scanner) {
+        System.out.print(message);
+        return scanner.nextLine();
     }
 }
